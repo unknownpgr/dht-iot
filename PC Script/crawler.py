30c755c008b71ddce11cbc87db1de6b3711fa611
unknownpgr <unknownpgr@gmail.com>
2021-02-22 01:40:23 +0900

    Add annotations and rename inconsistent variables

--- PC Script/crawler.py
+++ PC Script/crawler.py
@@ -5,39 +5,65 @@ import socket
 import csv
 import datetime
 
+# Constants
 HOST = '192.168.137.47'
 PORT = 8080
-key = '5766457145756e6b3835484a417a52'
-link_id = '1220003800'
-url = f"http://openapi.seoul.go.kr:8088/{key}/xml/TrafficInfo/1/5/{link_id}"
+API_KEY = '5766457145756e6b3835484a417a52'
+LINK_ID = '1220003800'
+
+# OpenAPI url for traffic data
+url = f"http://openapi.seoul.go.kr:8088/{API_KEY}/xml/TrafficInfo/1/5/{LINK_ID}"
 
 with open('output.csv', 'w', newline='') as f:
+    # CSV file for data logging
     csv_writer = csv.writer(f)
     csv_writer.writerow(['Timestamp', 'Humidity', 'Temperature', 'Traffic'])
+
+    # Start main process
     while True:
         print('-----------------------')
+
+        # Get the data from the Arduino Yun
         try:
+            # Try connecting to Arduino and get data
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 s.connect((HOST, PORT))
                 data_arduino = str(s.recv(1024), encoding='utf-8')
-                print(data_arduino)
+                # Split the data into humidity and temperature
                 data_humidity, data_temperature = data_arduino.split('/')
                 print('Arduino Data :', data_arduino)
         except (ConnectionRefusedError, ConnectionResetError):
+            # If connection is not established, print error message and try again after 2 seconds.
             print('Could not connect to Arduino. Try again after 2 seconds.')
             time.sleep(2)
             continue
 
+        # Get the traffic data from OpenAPI
         data = requests.get(url).text
+
+        # Parse xml-type data with ElementTree library
         root = ET.fromstring(data)
+
+        # Get response code
         code = root[1][0].text
+
         if code != 'INFO-000':
+            # If response contains errors, print it and retry.
             print(f'Could not get data with OpenAPI. :', root[1][1].text)
             print("Try again after 2 seconds.")
-        else:
-            data_traffic = root[2][1].text
-            print('OpenAPI Data :', data_traffic)
+            time.sleep(2)
+            continue
+
+        # Else, just parse data and get required value(traffic)
+        data_traffic = root[2][1].text
+        print('OpenAPI Data :', data_traffic)
+
+        # Concatenate data and write it to csv file
         csv_writer.writerow(
             [datetime.datetime.now(), data_humidity, data_temperature, data_traffic])
+
+        # Flush data
         f.flush()
+
+        # Wait for 2 seconds
         time.sleep(2)
